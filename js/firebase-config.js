98ec6bab84c1a3e496335f885dd616a6aec7d6bb
Bilal Ahmad <irathernotm8@gmail.com>
2026-07-15 22:22:29 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,10 +1,11 @@
 // Paste the config object from your Firebase project here.
 // Firebase console -> Project settings -> General -> Your apps -> SDK setup and configuration
 const firebaseConfig = {
-  apiKey: "PASTE_YOUR_API_KEY_HERE",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  projectId: "PASTE_YOUR_PROJECT_ID",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+    apiKey: "AIzaSyCKfA_36vOxG8rnaFzejArVoSh1RUqgUMQ",
+    authDomain: "football-2178.firebaseapp.com",
+    projectId: "football-2178",
+    storageBucket: "football-2178.firebasestorage.app",
+    messagingSenderId: "224236417773",
+    appId: "1:224236417773:web:7deb48514f65e2e2e552e4",
+    measurementId: "G-6RC9PPG6JM"
 };
